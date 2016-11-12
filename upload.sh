ad2a0a262061b020067259bb38e529a41a7cc2e7
rs <rs@rs.rs>
2016-11-13 01:18:34 +0200

    aaaa

---
+++ upload.sh
@@ -0,0 +1,5 @@
+aws s3 sync ./css s3://beta.fanstation.co.il/css
+aws s3 sync ./img s3://beta.fanstation.co.il/img
+aws s3 sync ./js s3://beta.fanstation.co.il/js
+aws s3 sync ./vendor s3://beta.fanstation.co.il/vendor
+aws s3 cp ./index.html s3://beta.fanstation.co.il/
